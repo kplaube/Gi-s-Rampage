37922ce3a4a82e13a89b8b573af5a24f4f8186c2
Klaus Laube <klaus@corp.globo.com>
2015-07-28 23:52:47 -0300

    Finishing the gameplay of the level 2

--- src/game/level2.lua
+++ src/game/level2.lua
@@ -10,6 +10,7 @@ local dusk = require( "Dusk.Dusk" )
 
 local blink = require( "libs.blink" )
 local TextDialog = require( "libs.dialog" )
+local StartLevel = require( "libs.start-level" )
 
 local Giselli = require( "game.chars.giselli" )
 local Guardian = require(  "game.chars.guardian2" )
@@ -26,9 +27,18 @@ level.sceneDialogs = {
         "Guardião: Seu noivo? *hic* Ele passou por aqui sim...",
         "Guardião: Mas estava muito bêbado e tentando usar o celular.",
         "Guardião: Acabou dormindo sentado, e eu o mandei para a próxima dimensão.",
-        "Guardião: Eu só permitirei que você prossiga se vencer o meu desafio.",
-        "Guardião: Você pode seguir se me ajudar a tomar 25 melzinhos...",
-        "Guardião: Valendo!"
+        "Guardião: Eu só permitirei que você prossiga se vencer o meu desafio. *hic*",
+        "Guardião: Você pode seguir se me ajudar a tomar 20 melzinhos...",
+        "Guardião: Valendo!",
+        "Guardião: *hic*"
+    },
+    [2] = {
+        "Guardião: Muito bem! *hic*",
+        "Guardião: Até parece que você já fez isso antes...",
+        "Guardião: Por gratidão te darei essa habilidade.",
+        "Giselli aprendeu a arte do debate.",
+        "Guardião: Muito *hic* obrigado, e boa sorte!",
+        "Giselli debate sobre os fenômenos subjetivos da ciência noética e converte o Guardião."
     }
 }
 
@@ -60,7 +70,7 @@ end
 
 function level:createGuardian()
     local guardian = Guardian.new()
-    guardian.x, guardian.y = 270, 140
+    guardian.x, guardian.y = 270, 135
 
     self.map.layer["characters"]:insert(guardian)
 
@@ -106,6 +116,146 @@ function level.beforeFirstDialog()
 end
 
 function level.onFirstDialogEnds()
+    level.startText:show()
+end
+
+function level.gameplayStart()
+    level:createObjects()
+    level:startHud()
+end
+
+function level.startHud()
+        level.hud = display.newGroup()
+
+    local rect = display.newRoundedRect(
+        level.hud, 0, 0, 120, 40, 4 )
+
+    level.hud.anchorX = 0
+    level.hud.anchorY = 0
+    level.hud.x = display.contentWidth - 70
+    level.hud.y = 30
+
+    level.hud.label = display.newText{
+        parent = level.hud,
+        text = "Melzinhos: ",
+        x = 10, y = 0,
+        font = native.systemFont,
+        fontSize = 14,
+        width = rect.contentWidth
+    }
+    level.hud.counter = display.newText{
+        parent = level.hud,
+        text = "20",
+        x = -10, y = 0,
+        font = native.systemFont,
+        fontSize = 14,
+        width = rect.contentWidth,
+        align = "right"
+    }
+
+    rect:setFillColor{
+        type = "gradient",
+        color1={ 1/255, 1/255, 1/255, 0.9 },
+        color2={ 1/255, 1/255, 1/255, 0.6 },
+        direction="down"
+    }
+end
+
+function level.destroyHud()
+    level.hud.isVisible = false
+    level.hud:remove()
+end
+
+function level:createObjects()
+    local beverage = display.newImageRect(
+        "images/objects/beverage.png",
+        32, 32)
+
+    beverage.anchorX = 0
+    beverage.anchorY = 0
+
+    beverage.positions = {
+        {x=35, y=35},
+        {x=352, y=160},
+        {x=192, y=128},
+        {x=64, y=224},
+        {x=448, y=128},
+        {x=0, y=224},
+        {x=128, y=96},
+        {x=448, y=288},
+        {x=35, y=35},
+        {x=192, y=224},
+        {x=320, y=128},
+        {x=448, y=192},
+        {x=256, y=224},
+        {x=192, y=0},
+        {x=35, y=35},
+        {x=384, y=256},
+        {x=352, y=160},
+        {x=192, y=128},
+        {x=128, y=96},
+        {x=448, y=288}
+    }
+    beverage.positionIndex = 1
+
+    function beverage.updatePosition()
+        beverage.x = beverage.positions[beverage.positionIndex].x
+        beverage.y = beverage.positions[beverage.positionIndex].y
+    end
+
+    beverage.updatePosition()
+    transition.blink( beverage, {time=1000})
+
+    beverage:addEventListener( "tap", level.onBeverageTap )
+    self.map.layer["characters"]:insert(beverage)
+end
+
+function level.onBeverageTap( event )
+    local counter = tonumber(level.hud.counter.text) - 1
+
+    audio.play( level.rightAnswerSound )
+    level.hud.counter.text = counter
+
+    if counter > 0 then
+        event.target.positionIndex = event.target.positionIndex + 1
+        event.target.updatePosition()
+
+        return false
+    end
+
+    event.target.isVisible = false
+
+    level:dispatchEvent{
+        name="endGameplay",
+        target=level
+    }
+end
+
+function level.gameplayEnd( event )
+    audio.stop( level.backgroundMusicChannel )
+    level.victoryMusicChannel = audio.play( level.victoryMusic, {
+        channel=1,
+        loops=-1
+    } )
+
+    level.destroyHud()
+    level:setDialog( level.sceneDialogs[2], level.onSecondDialogEnds )
+
+    timer.performWithDelay( 500, function()
+        level.textDialog:startDialog()
+    end )
+end
+
+function level.onSecondDialogEnds()
+    blink.blinkScreen(function()
+        level.gi.isVisible = false
+
+        timer.performWithDelay( 500, level.endLevel)
+    end )
+end
+
+function level.endLevel()
+    composer.gotoScene( "game.level3-intro", "fade", 500 )
 end
 
 -----------------------------------------------------------------------------------------
@@ -118,10 +268,19 @@ function scene:create( event )
     level:createGuardian()
     level:createCrowd()
 
+    level.backgroundMusic = audio.loadStream( "musics/level2.mp3" )
+    level.victoryMusic = audio.loadStream( "musics/victory.mp3" )
+
     level:setDialog( level.sceneDialogs[1], level.onFirstDialogEnds )
+    level.startText = StartLevel.new()
+    level.startText:addEventListener( "hideText", level.gameplayStart )
+    level:addEventListener( "endGameplay", level.gameplayEnd )
+
+    level.rightAnswerSound = audio.loadSound( "sounds/right.mp3" )
 
     sceneGroup:insert( level.map )
     sceneGroup:insert( level.textDialog )
+    sceneGroup:insert( level.startText )
 end
 
 function scene:show( event )
@@ -131,19 +290,40 @@ function scene:show( event )
         return
     end
 
+    level.backgroundMusicChannel = audio.play( level.backgroundMusic, {
+        channel=1,
+        loops=-1
+    } )
+
     level:startLevel()
 end
 
+function scene:hide( event )
+    local phase = event.phase
+
+    if phase == "will" then
+        audio.stop( level.victoryMusicChannel )
+    end
+end
+
 function scene:destroy( event )
+    level.backgroundMusic = nil
+    level.backgroundMusicChannel = nil
+    level.victoryMusic = nil
+    level.victoryMusicChannel = nil
     level.map = nil
     level.gi = nil
     level.guardian = nil
+    level.textDialog = nil
+    level.startText = nil
+    level.sceneDialogs = nil
 end
 
 -----------------------------------------------------------------------------------------
 
 scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
 
 -----------------------------------------------------------------------------------------
